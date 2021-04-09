f836e8ff90523b48f0280c7729e1fb4987a78bd4
Cezar Ionescu <cezar.ionescu@3pillarglobal.com>
2021-04-09 22:59:51 +0300

    + Display values in templates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+
   return (
     <div className="App">
-      <div className="conte">
-        <h1>App component</h1>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
       </div>
     </div>
   );
