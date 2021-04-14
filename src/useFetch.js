a0ad6a18d12fef3825051ac135c46fe60475ebf3
Cezar Ionescu <cezar.ionescu@3pillarglobal.com>
2021-04-14 17:59:29 +0300

    + Created a custom hook (basically separating reusable business logic in a separate file)

---
+++ src/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+const useFetch = (url) => {
+
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for the resource')
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setData(data);
+                setIsPending(false);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, [url]);
+
+    return { data, isPending }
+}
+
+export default useFetch;
