0376ab844001f9108711c6ec567a8f382ddf4f44
Cezar Ionescu <cezar.ionescu@3pillarglobal.com>
2021-04-15 18:09:06 +0300

    + Used AbortController() to cancel a fetch when is still running and the component is unmounted (clean-up)

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,20 +5,31 @@ const useFetch = (url) => {
     const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch(url)
-            .then((res) => {
-                if (!res.ok) {
-                    throw Error('Could not fetch the data for the resource')
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setData(data);
-                setIsPending(false);
-            })
-            .catch((err) => {
-                console.log(err.message);
-            })
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then((res) => {
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for the resource')
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                })
+                .catch((err) => {
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch aborted');
+                    } else {
+                        setIsPending(false);
+                        console.log(err.message);
+                    }
+                })
+        }, 1000);
+
+        return () => { abortCont.abort() }
     }, [url]);
 
     return { data, isPending }
